f3a5b485ebf4fe83a38f5a6e631450f3f60de336
Quaad <41535952+Quaad@users.noreply.github.com>
2022-10-19 17:38:27 +0100

    Score implement
    Added code to increment score when moving over yellow tiles

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ const canvas = document.querySelector('#myCanvas');
 // get the canvas context
 let ctx = canvas.getContext('2d');
 
-let count = document.getElementById('counter');
+let points = document.getElementById('points');
 let health = document.getElementById('health')
 
 // starting coordinates
 let playerX=100
 let playerY=0
-let counter=1
-count.innerText = "Score: " + counter
+let score=0
+points.innerText = "Score: " + score
 health.innerText = "Health: " + 3
 
 // maze data 
-let row = [
+let map = [
     [1,1,0,1,1,1,1,1,1,1,1,1],
     [1,0,0,1,0,0,0,0,0,0,0,1],
     [1,1,2,1,0,1,1,0,1,1,0,1],
@@ -30,12 +30,12 @@ let row = [
 ]
 
 // Maze generator
-for (let r = 0; r < row.length; r++) {
-    for(let c = 0; c < row[0].length; c++) {
+for (let r = 0; r < map.length; r++) {
+    for(let c = 0; c < map[0].length; c++) {
         ctx.fillStyle = 'blue';
-        if(row[r][c]==1) {
+        if(map[r][c]==1) {
             ctx.fillRect((c*50), (r*50), 50, 50);
-        } else if (row[r][c]==2) {
+        } else if (map[r][c]==2) {
             ctx.fillStyle = 'Yellow';
             ctx.fillRect((c*50), (r*50), 50, 50);
         }
@@ -45,7 +45,7 @@ for (let r = 0; r < row.length; r++) {
 // Drawing the player and checking if they've reached the end
 function drawPlayer() {
     // set fill for blocks
-    ctx.fillStyle = 'red';
+    ctx.fillStyle = 'green';
     // draw a rectangle with fill 
     ctx.fillRect(playerX, playerY, 50, 50);
     // ctx.strokeRect(50, 50, 150, 100);
@@ -116,9 +116,21 @@ function moveDown() {
     var imgd = ctx.getImageData(playerX, playerY+75, 25, 25);
     var pix = imgd.data;
     let blue = 0;
+    let yellow = 0;
     if(pix[0] == 0 && pix[1] == 0 && pix[2] == 255) {
         console.log("that's blue")
         blue = 1;
+    } else if(pix[0] == 255 && pix[1] == 255 && pix[2] == 0) {
+        console.log("that's yellow")
+        yellow = 1
+    }
+    if(playerY<550 && yellow) {
+        removePlayer()
+        playerY=playerY+50
+        drawPlayer()
+        score+= 50
+        points.innerText = "Score: " + score
+        return
     }
     if (playerY<550  && !blue) {
         removePlayer()
@@ -133,7 +145,7 @@ function moveLeft() {
     let blue = 0;
     if(pix[0] == 0 && pix[1] == 0 && pix[2] == 255) {
         console.log("that's blue")
-        blue = 1;
+        blue = 1
     }
     if (playerX>0  && !blue) {
         removePlayer()
